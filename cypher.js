8aed72326d5ebe110c9cdfc64779dc275dd8d826
Gustavo Costa <gusbemacbe@gmail.com>
2020-01-08 18:21:58 -0300

    Updating in reference to #5, #6 and #6

--- cypher.js
+++ cypher.js
@@ -34,8 +34,14 @@ function hljsDefineCypher(hljs)
         begin: /(\s+|,)\w+\(/,
         end: /\)/,
         keywords: {
-          built_in: 'all any exists none single coalesce endNode head id last length properties size startNode timestamp toBoolean toFloat toInteger type avg collect count max min percentileCont percentileDisc stDev stDevP sum extract filter keys labels nodes range reduce relationships reverse tail abs ceil floor rand round sign e exp log log10 sqrt acos asin atan atan2 cos cot degrees haversin pi radians sin tan left ltrim replace reverse right rtrim split substring toLower toString toUpper trim distance'
+          built_in: 'all call any exists none single coalesce endNode head id last length properties size startNode timestamp toBoolean toFloat toInteger type avg collect count max min percentileCont percentileDisc stDev stDevP sum extract filter keys labels nodes range reduce relationships reverse tail abs ceil floor rand round sign e exp log log10 sqrt acos asin atan atan2 cos cot degrees haversin pi radians sin tan left ltrim replace reverse right rtrim split substring toLower toString toUpper trim distance'
         }
+      },
+      hljs.C_BLOCK_COMMENT_MODE,
+      hljs.C_LINE_COMMENT_MODE,
+      {
+        begin: '//',
+        ends: '//',
       }
     ]
   }
