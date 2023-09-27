284e544bbc648c61791133cc0687fa847c7028a2
Stefano Ottolenghi <stefano.ottolenghi@neo4j.com>
2023-09-27 14:02:05 +0200

    Update keywords to v5 and improve lexemes breakup.

--- src/cypher.js
+++ src/cypher.js
@@ -10,8 +10,9 @@ module.exports = function (hljs)
     case_insensitive: true,
     keywords:
       {
-        keyword: 'as asc ascending assert by call case commit constraint create csv cypher delete desc descending detach distinct drop else end ends explain fieldterminator foreach from headers in index is join limit load match merge on optional order periodic profile remove return scan set skip start starts then union unique unwind using when where with yield',
-        literal: 'true false null'
+        keyword: 'as asc ascending and assert by call case collect commit constraint create csv cypher delete desc descending detach distinct drop else end ends explain fieldterminator for foreach from headers in index is join limit load match merge not on optional order periodic profile remove return scan set skip start starts then union unique unwind use using when where with yield',
+        literal: 'true false null',
+        $pattern: /(?<=^|\s)[a-zA-Z0-9_\.]+(?=$|\s)/  // allows to better exclude keywords-like matches to be highlighted when not acting as keywords
       },
     contains:
       [
@@ -34,10 +35,10 @@ module.exports = function (hljs)
         },
         {
           className: 'functionCall',
-          begin: /(\s+|,)\w+\(/,
+          begin: /(\s+|,)[a-zA-Z0-9_\.]+\(/,
           end: /\)/,
           keywords: {
-            built_in: 'all any exists none single coalesce endNode head id last length properties size startNode timestamp toBoolean toFloat toInteger type avg collect count max min percentileCont percentileDisc stDev stDevP sum extract filter keys labels nodes range reduce relationships reverse tail abs ceil floor rand round sign e exp log log10 sqrt acos asin atan atan2 cos cot degrees haversin pi radians sin tan left ltrim replace reverse right rtrim split substring toLower toString toUpper trim distance'
+            built_in: 'all any exists isEmpty none single coalesce elementId endNode head id last length properties randomUUID size startNode timestamp toBoolean toBooleanOrNull toFloat toFloatOrNull toInteger toIntegerOrNull type valueType avg collect count max min percentileCont percentileDisc stDev stDevP sum extract filter keys labels nodes range reduce relationships reverse tail toBooleanList toFloatList toIntegerList toStringList abs ceil floor isNaN rand round sign e exp log log10 sqrt acos asin atan atan2 cos cot degrees haversin pi radians sin tan left ltrim replace reverse right rtrim split substring toLower toString toStringOrNull toUpper trim date datetime localdatetime localtime time duration duration duration duration duration point linenumber file graph db'
           }
         },
         hljs.C_BLOCK_COMMENT_MODE,
