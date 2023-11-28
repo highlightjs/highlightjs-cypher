a87ebd69ad9faf9f782e5500b9c8c4277954d974
Stefano Ottolenghi <stefano.ottolenghi@neo4j.com>
2023-11-28 05:26:16 +0100

    up to 5.14

--- src/cypher.js
+++ src/cypher.js
@@ -10,7 +10,7 @@ module.exports = function (hljs)
     case_insensitive: true,
     keywords:
       {
-        keyword: 'as asc ascending and assert by call case collect commit constraint create csv cypher delete desc descending detach distinct drop else end ends explain fieldterminator for foreach from headers in index is join limit load match merge not on optional order periodic profile remove return scan set skip start starts then union unique unwind use using when where with yield',
+        keyword: 'as asc ascending and assert by call case collect commit constraint create csv cypher delete desc descending detach distinct drop else end ends explain fieldterminator for foreach from headers in index is join limit load match merge nodetach not on optional order periodic profile remove return scan set skip start starts then union unique unwind use using when where with yield',
         literal: 'true false null',
         $pattern: /(?<=^|\s)[a-zA-Z0-9_\.]+(?=$|\s)/  // allows to better exclude keywords-like matches to be highlighted when not acting as keywords
       },
@@ -38,7 +38,7 @@ module.exports = function (hljs)
           begin: /(\s+|,)[a-zA-Z0-9_\.]+\(/,
           end: /\)/,
           keywords: {
-            built_in: 'all any exists isEmpty none single coalesce elementId endNode head id last length properties randomUUID size startNode timestamp toBoolean toBooleanOrNull toFloat toFloatOrNull toInteger toIntegerOrNull type valueType avg collect count max min percentileCont percentileDisc stDev stDevP sum extract filter keys labels nodes range reduce relationships reverse tail toBooleanList toFloatList toIntegerList toStringList abs ceil floor isNaN rand round sign e exp log log10 sqrt acos asin atan atan2 cos cot degrees haversin pi radians sin tan left ltrim replace reverse right rtrim split substring toLower toString toStringOrNull toUpper trim date datetime localdatetime localtime time duration duration duration duration duration point linenumber file graph db'
+            built_in: 'all any exists isEmpty none single coalesce elementId endNode head id last length properties randomUUID size startNode timestamp toBoolean toBooleanOrNull toFloat toFloatOrNull toInteger toIntegerOrNull type valueType avg collect count max min percentileCont percentileDisc stDev stDevP sum extract filter keys labels nodes range reduce relationships reverse tail toBooleanList toFloatList toIntegerList toStringList abs ceil floor isNaN rand round sign e exp log log10 sqrt acos asin atan atan2 cos cot degrees haversin pi radians sin tan left ltrim replace reverse right rtrim split substring toLower toString toStringOrNull toUpper trim date datetime localdatetime localtime time duration duration duration duration duration point linenumber file graph db cdc char_length character_length'
           }
         },
         hljs.C_BLOCK_COMMENT_MODE,
